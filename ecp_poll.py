86cca6cf1d8db4c27f351715e1c7ac77e8671655
Duong Do <dohoangduong95@gmail.com>
2018-03-02 17:15:27 -0600

    Add document to the script of Brief Google Form

--- ecp_poll.py
+++ ecp_poll.py
@@ -1,12 +1,12 @@
 # required install package
 # pip install oauth2client==1.5.2
 # pip install gspread==0.6.2
-# pip install cryptography==1.4
 
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 import json
 
+# Below are the indexes of necessary information in the Google Sheet. Need to change if needed
 
 FIRST_NAME = 1
 LAST_NAME = 2
@@ -17,6 +17,7 @@ SPECIAL_REQUIREMENT = 11
 DETAIL_REQUIREMENT = 12
 
 
+# get the google client - no need to change
 def get_google_client():
     with open('google_key.json') as f:
         config = json.load(f)
@@ -29,6 +30,7 @@ def get_google_client():
     return gspread.authorize(creds)
 
 
+# group by technical area
 def process_value(values, topics):
     topic = values[TOPIC]
     if topic in topics:
@@ -50,16 +52,25 @@ def get_name(values):
     return values[FIRST_NAME] + " " + values[LAST_NAME]
 
 
+# write content to file
 def write_to_file(topics):
     with open("ecp.md", "w") as f:
         for topic in sorted(topics.keys()):
+
+            # Write header for table
             f.write("\n"+topic+"\n")
             f.write("Project | POC | Response\n")
             f.write("-- | -- | --\n")
+
             ls = []
+            # add subtopics and author name to list
             for values in topics[topic]:
                 ls.append([get_sub_topic(values), get_name(values)])
+
+            # sort the list by project orders
             ls = sorted(ls, key=lambda x: x[0])
+
+            # write the list to file
             for x in ls:
                 f.write(x[0] + " | " + x[1] + " | " + "Response" + "\n")
 
@@ -81,7 +92,10 @@ def run():
         # Blank row
         if not values[0]:
             break
+        # process the values collected
         process_value(values, topics)
+
+    # write values into file
     write_to_file(topics)
 
 
