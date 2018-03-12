8857e1bd87232ec4e14c0e80ccbff53da6718cb5
Duong Do <dohoangduong95@gmail.com>
2018-03-11 20:57:01 -0500

    Add script file

---
+++ ecp_technical_report.py
@@ -0,0 +1,105 @@
+# required install package
+# pip install oauth2client==1.5.2
+# pip install gspread==0.6.2
+
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+import json
+
+# Below are the indexes of necessary information in the Google Sheet. Need to change if needed
+
+TECH_AREA = 4 # Column E in the spreadsheet (not sure if 5 is the right number, so fix if not)
+PROJECT = range(5, 10)       # will be one of the columns F through J in the spreadsheet
+HAVE_SPOTLIGHT = 10  # column K
+SPOTLIGHT_REPORT = 11  # column L
+PROGRESS_REPORT = 12  # column M
+
+
+# get the google client - no need to change
+def get_google_client():
+    with open('google_key.json') as f:
+        config = json.load(f)
+
+    creds = SignedJwtAssertionCredentials(
+        config['client_email'],
+        config['private_key'],
+        ['https://spreadsheets.google.com/feeds']
+    )
+    return gspread.authorize(creds)
+
+
+# group by technical area
+def process_value(values, topics):
+    topic = values[TECH_AREA]
+    if topic in topics:
+        topics[topic].append(values)
+    else:
+        topics[topic] = [values]
+
+
+# get the project from Google sheet
+def get_project(values):
+    for i in PROJECT:
+        if values[i]:
+            return values[i]
+    return ""
+
+
+#def get_name(values):
+#    return values[FIRST_NAME] + " " + values[LAST_NAME]
+
+
+# write content to file
+def write_to_file(topics):
+    for topic in sorted(topics.keys()):
+        filename = "{}.md".format(topic)
+        with open(filename, "w") as f:
+            # Write header for table
+            f.write("### {}\n\n".format(topic))
+
+            ls = []
+            # add subtopics and author name to list
+            for values in topics[topic]:
+                ls.append([get_project(values), values[SPOTLIGHT_REPORT], values[PROGRESS_REPORT]])
+
+            # sort the list by project orders
+            ls = sorted(ls, key=lambda x: x[0])
+
+            # write the list to file
+            for project, spotlight, progress_report in ls:
+                if spotlight:
+                    f.write("Spotlight: {}\n".format(spotlight))
+                f.write("{}\n".format(project))
+                if progress_report:
+                    f.write("{}\n\n".format(progress_report))
+                else:
+                    f.write("No report")
+
+
+def run():
+    gc = get_google_client()
+
+    # Open file: key in the URL
+    doc = gc.open_by_key('1wRWjy5UvA62hNvdhU-JVsWK0_8r6exdnDyn4vufand0')
+
+    # Open tab
+    sheet = doc.worksheet("Sheet1")
+
+    row_count = sheet.row_count
+    topics = {}
+    for row in range(2, row_count + 1):
+        values = sheet.row_values(row)
+
+        # Blank row
+        if not values[0]:
+            break
+        # process the values collected
+        process_value(values, topics)
+        #print values
+
+    # write values into file
+    write_to_file(topics)
+
+
+if __name__ == '__main__':
+    run()
